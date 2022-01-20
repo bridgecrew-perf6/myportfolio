3f86eeda0acf9b49f387f43503b5efcc20b4c0fe
oyenolaayomide <oyenolaayomide87@gmail.com>
2022-01-20 14:32:20 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://pirel.ml/user/phil",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://pirel.ml/user/phil",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://pirel.ml/user/phil",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pirel.ml/user/phil",
+  },
+  {
+    title: "Passenger counter app",
+    subtitle: "Javascript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-5.gif",
+    link: "https://pirel.ml/user/phil",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Oyenola Philip has been building one of the best websites for me and I have always loved it. He has been the one building and managing our company sites and I have always been in love of it. Oyenola Philip is one of the best full stack web developers I have ever seen.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Oyenola Philip has been building one of the best websites for me and I have always loved it. He has been the one building and managing our company sites and I have always been in love of it. Oyenola Philip is one of the best full stack web developers I have ever seen.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "Html",
+  "Css",
+  "JavaScript",
+  "Jquery",
+  "Sass",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "Bootstrap",
+  "Redux",
+  "Firebase",
+  "Apollo",
+];
